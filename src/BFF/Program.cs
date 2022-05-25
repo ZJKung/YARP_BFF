e99f9e0e1526e97ac894ee866342b8db0e6807c8
zjkung1123 <zjkung1123@fugle.tw>
2022-05-25 17:53:05 +0800

    init

--- src/BFF/Program.cs
+++ src/BFF/Program.cs
@@ -1,9 +1,13 @@
+using Yarp;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,8 +16,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapReverseProxy();
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
